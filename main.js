0bb3be355d3a71eb3f480a5151e3c0e8ec7d5575
Mahfujur Rahman <mahfujurrahman6793@gmail.com>
2023-10-28 14:30:35 +0600

    backspace and factorial functionalities done

--- main.js
+++ main.js
@@ -14,9 +14,20 @@ for (item of btn) {
             btnText = '*';
         }
 
-        display.value += btnText;
-        
-        
+        display.value += btnText;   
     })
 }
 
+const backspc = () => {
+    display.value = display.value.substr(0, display.value.length - 1);
+}
+const fact = () => {
+    let f = 1;
+    let i;
+    const num = display.value;
+    for (i = 1; i <= num; i++){
+        f = i * f;
+    }
+    i -= 1;
+    display.value = f;
+}
